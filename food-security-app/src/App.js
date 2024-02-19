892a2d81705b375b7a7dd40aabab41629a09503c
sarathsaravanan <143551683+sarathsaravanan@users.noreply.github.com>
2024-02-19 12:04:00 -0800

    commiting this

--- food-security-app/src/App.js
+++ food-security-app/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, {useRef, useEffect, useState} from "react";
+
+// function App() {
+//   const videoRef = useRef(null);
+//   const photoRef = useRef(null);
+//   const [hasPhoto, setHasPhoto] = useState(false);
+
+//   const getVideo = () => {
+//     navigator.mediaDevices.getUserMedia({video: { width: 1920, height: 1080 } })
+
+//     .then(stream => {
+//       let video = videoRef.current;
+//       video.srcObject = stream;
+//       video.play();
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     })
+//   }
+
+//   useEffect(() => {
+//     getVideo();
+//   }, [videoRef]);
+
+
+//   return (
+//     <div className="App">
+//       <div className="camera">
+//         <video ref={videoRef}>
+//           <button>SNAP!</button>
+//         </video>
+//       </div>
+//       <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+//         <canvas ref={photoRef}></canvas>
+//         <button>Close</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useRef, useEffect, useState } from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const [hasPhoto, setHasPhoto] = useState(false);
+
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: { width: 1920, height: 1080 } })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  const takePhoto = () => {
+    let video = videoRef.current;
+    let photo = photoRef.current;
+    const context = photo.getContext("2d");
+
+    context.drawImage(video, 0, 0, 1920, 1080);
+    setHasPhoto(true);
+  };
+
+  const closePhoto = () => {
+    setHasPhoto(false);
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button onClick={takePhoto}>SNAP!</button>
+      </div>
+      <div className={`result ${hasPhoto ? 'hasPhoto' : ''}`}>
+        <canvas ref={photoRef}></canvas>
+        <button onClick={closePhoto}>Close</button>
+      </div>
     </div>
   );
 }
